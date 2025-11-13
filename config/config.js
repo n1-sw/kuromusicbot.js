ad6323e6cb36942286c70106971e5ccf382af1d9
𝗔𝗹𝗲𝘅𝗮𝗻.⁷⁷⁷ <87300812+n1-sw@users.noreply.github.com>
2025-11-13 13:19:16 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-  token: process.env.DISCORD_TOKEN || 'MTQwNDI3MzA5OTEyNTI5NzIxMg.GmsViN.PxZImI4_G6U9enGBXgHtntL8CYDxGVt-FTMZ48',
-  clientId: process.env.CLIENT_ID || '1404273099125297212',
-  ownerId: process.env.OWNER_ID || '698131338846404609',
+  token: process.env.DISCORD_TOKEN || '',
+  clientId: process.env.CLIENT_ID || '',
+  ownerId: process.env.OWNER_ID || '',
   prefix: process.env.PREFIX || '!',
   spotifyClientId: process.env.SPOTIFY_CLIENT_ID || '14b939dcdf0c409ba048fee293da4719',
   spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET || '06f9cfa7bd7b4c89a09424f410f362a7',
